cdb4bdf23199de287ca341906088cab0225a4639
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-22 20:03:20 +0200

    to-do-list structure

--- src/index.js
+++ src/index.js
@@ -4,14 +4,53 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function ToDoTable(props){
+  return (
+    <h1>Todo</h1>
+  )
+}
+
+class ToDoForm extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      toDos: []
+    };
+  }
+
+  render() {
+    <div>
+      <form>
+        <label>
+            Enter to-do:
+            <input type="text" />
+        </label>
+
+        <input type="submit" />
+      </form>
+
+      <ToDoList list={this.state.toDos} />
+    </div>
+  }
+}
+
+function ToDoList(props){
+  return (
+    <ul>
+      {props.map(toDo => <ToDoListItem toDoItem={toDo} />)}
+    </ul>
+  )
+}
+
+function ToDoListItem(props){
+  
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ToDoTable />
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
