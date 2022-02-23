9dacaef2c53370259a26b63c3db631f2e2e41dbc
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-23 20:14:06 +0200

    redo

--- src/index.js
+++ src/index.js
@@ -4,51 +4,70 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function ToDoTable(props){
-  return (
-    <h1>Todo</h1>
-  )
-}
-
-class ToDoForm extends React.Component{
+class ToDoApp extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
       toDos: []
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //this.complete = this.complete.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    this.setState(prevState => ({
+        toDos: prevState.toDos.concat({item: event.target["enteredToDo"].value, completed: false})
+      }))
+  }
+
+  complete(itemName) {
+      return "complete";
   }
 
   render() {
-    <div>
-      <form>
-        <label>
-            Enter to-do:
-            <input type="text" />
-        </label>
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <label>
+                Enter to-do:
+                <input type="text" name="enteredToDo"/>
+            </label>
 
-        <input type="submit" />
-      </form>
+            <input type="submit" />
+          </fieldset>
+        </form>
 
-      <ToDoList list={this.state.toDos} />
-    </div>
-  }
+        <ToDoList completeFunc={this.complete} list={this.state.toDos} />
+      </div>      
+    )
+  }  
 }
 
 function ToDoList(props){
   return (
     <ul>
-      {props.map(toDo => <ToDoListItem toDoItem={toDo} />)}
+      {props.list.map(toDo => <ToDoListItem key={toDo} completeFunc={props.completeFunc} toDoItem={toDo} />)}
     </ul>
   )
 }
 
 function ToDoListItem(props){
-  
+  let completed = "";
+
+  return (
+    <li>
+      <p>{props.toDoItem.item}</p> <button onClick={completed = props.completeFunc(props.toDoItem.item)}>Completed</button> <button>Deleted</button>
+    </li>
+  )
 }
 
 ReactDOM.render(
-  <ToDoTable />
+  <ToDoApp />
   ,
   document.getElementById('root')
 );
